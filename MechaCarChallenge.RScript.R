dfb3c4b792c8a501247af3fe899b5cf23d4bdd20
guevarch <107594143+guevarch@users.noreply.github.com>
2022-09-24 15:21:50 -0400

    add

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,12 +1,10 @@
+## Deliverable 1
 setwd("C:/Users/16474/Desktop/Coding/MechaCar_Statistical_Analysis")
 library(dplyr)
 library(tidypredict)
 df1 <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~vehicle_weight+spoiler_angle+ground_clearance+AWD+vehicle_length, data=df1)
 summary(lm(mpg ~vehicle_weight+spoiler_angle+ground_clearance+AWD+vehicle_length, data=df1)) #summarize linear model
-df2 <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-total_summary <- df2 %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
-lot_summary <- df2 %>% group_by(Manufacturing_Lot) %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
 
 install.packages("ggplot2")
 library(ggplot2)
@@ -42,4 +40,8 @@ yvals <- model$coefficients['mpg']*df1$mpg + model$coefficients['(Intercept)'] #
 plt <- ggplot(df1,aes(x=mpg,y=spoiler_angle)) #import dataset into ggplot2
 plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and linear mode
 
+## Deliverable 2
+df2 <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- df2 %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
+lot_summary <- df2 %>% group_by(Manufacturing_Lot) %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
 
