e8e7e39df124ede99b675378ff4fbb446086023a
guevarch <107594143+guevarch@users.noreply.github.com>
2022-09-24 11:16:36 -0400

    add

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,9 +1,10 @@
 setwd("C:/Users/16474/Desktop/Coding/MechaCar_Statistical_Analysis")
 df1 <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-lm(vehicle_length ~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg, data=df1)
-summary(lm(vehicle_length ~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg, data=df1)) #summarize linear model
+lm(vehicle_length ~mpg+spoiler_angle+ground_clearance+AWD+vehicle_weight+vehicle_length, data=df1)
+summary(lm(mpg ~vehicle_weight+spoiler_angle+ground_clearance+AWD+vehicle_length, data=df1)) #summarize linear model
 df2 <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- df2 %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
 lot_summary <- df2 %>% group_by(Manufacturing_Lot) %>% summarize(median=median(PSI), mean=mean(PSI), sd=sd(PSI), variance=var(PSI))
+
 
 
